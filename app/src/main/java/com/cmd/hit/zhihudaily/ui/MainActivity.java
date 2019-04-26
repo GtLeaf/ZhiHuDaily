7eeea3db52fca683e6d3c063f0da3450115e3647
[GtLeaf] <962755614@qq.com>
2019-04-26 20:58:48 +0800

    add folder

---
+++ app/src/main/java/com/cmd/hit/zhihudaily/ui/MainActivity.java
@@ -0,0 +1,19 @@
+package com.cmd.hit.zhihudaily.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cmd.hit.zhihudaily.R;
+
+/**
+ * Created by PC-0775 on 2019/4/26.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/cmd/hit/zhihudaily/viewModel/MainActivityModel.java
@@ -0,0 +1,10 @@
+package com.cmd.hit.zhihudaily.viewModel;
+
+/**
+ * Created by PC-0775 on 2019/4/26.
+ */
+
+public class MainActivityModel {
+
+    
+}
